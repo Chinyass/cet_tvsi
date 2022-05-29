4586b82721cbf40a94f19fbf0c23c081b2eece61
Chinyass <bodaral17r85@gmail.com>
2022-05-29 15:40:17 +0700

    rooms adjustment

--- node_socket/app.js
+++ node_socket/app.js
@@ -3,25 +3,24 @@ const io = require("socket.io")(3000,{
         origin: "*",
     }
 })
-const mySnmp = require('./mySnmp')
 
+//const mySnmp = require('./mySnmp')
 let rooms = []
 
 io.on('connection', (socket) => {
     console.log('a user connected');
     socket.on("get-traffic", (serial) => {
-        console.log(serial)
         socket.join(serial)
-
+    
         if (rooms.map( el => el.serial).includes(serial) ){
-            console.log("FINDED")
             const temp = []
             rooms.map( el => {
                 if (el.serial == serial){
                     temp.push({
                         serial,
-                        users: el.users + 1
+                        users: el.users.concat(socket.id)
                     })
+
                 }
                 else{
                     temp.push(el)
@@ -30,21 +29,27 @@ io.on('connection', (socket) => {
             rooms = [...temp]
         }
         else{
-            console.log("NOT FOUND")
+            console.log('room',serial,'created')
             rooms.push({
                 serial,
-                users: 1
+                users: [socket.id]
             })
         }
-        
     })
-});
+    socket.on("disconnecting", () => {
+        rooms.forEach( el => {
+            if (el.users.includes(socket.id) ){
+                const index = el.users.indexOf(socket.id);
+                el.users.splice(index, 1);
+            }
+        })
+    });
 
+});
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
-
 
 function sliceIntoChunks(arr, chunkSize) {
     const res = [];
@@ -55,20 +60,22 @@ function sliceIntoChunks(arr, chunkSize) {
     return res;
 }
 
-
 function convert_hex_to_dec(hexserial){
     hexserial = hexserial.replace('ELTX','454C5458')
     let arrhexserial = sliceIntoChunks(hexserial,2)
-    //arrhexserial = arrhexserial.map( el => el.join('') )
     return arrhexserial.map( el => parseInt(el, 16) ).join('.')
 }
 
-
 setInterval(() => {
+    rooms = rooms.filter( el => {
+        return el.users.length > 0 
+    })
     rooms.forEach( async (room) => {
-        let snmp = new mySnmp('10.3.0.35','private_set')
-        const dec_serial = convert_hex_to_dec(room.serial)
-        console.log(dec_serial)        
+        
+        console.log('sending data to ',room)
+        //let snmp = new mySnmp('10.3.0.35','private_set')
+        /*
+        const dec_serial = convert_hex_to_dec(room.serial)      
         const getRx = async () => {
             return await snmp.get([`1.3.6.1.4.1.35265.1.22.3.3.10.1.6.1.8.${dec_serial}.1.1`])
         }
@@ -78,7 +85,8 @@ setInterval(() => {
         let new_data = await getRx()
         const endtime = Math.ceil(Date.now() / 1000)
         rx = parseInt( ( ( parseInt(new_data) - parseInt(old_data) ) /  ( endtime - starttime  ) ) * 8 )
-        console.log('RX',rx)
+        */
+        let rx = 10
         io.to(room.serial).emit("send-traffic",rx)
     })    
-},13000)
+},2000)
